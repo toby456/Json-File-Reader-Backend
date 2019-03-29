03894f3e483a54355e0f788f21d3df410fb529ca
Toby Ellison-Scott <ellisonscotttoby@gmail.com>
2019-03-29 10:13:18 +0000

    Created repository for contact persistence by extending CrudRepository

--- src/main/java/com/toby/json/repository/ContactRepo.java
+++ src/main/java/com/toby/json/repository/ContactRepo.java
@@ -1,5 +1,11 @@
 package com.toby.json.repository;
 
-public class ContactRepo {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.toby.json.domain.Contact;
+
+@Repository
+public interface ContactRepo extends CrudRepository<Contact, String>{
 
 }
