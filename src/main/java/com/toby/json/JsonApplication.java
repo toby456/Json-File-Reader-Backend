293be470fb3516f0780aaf048fbc86e6c9214465
Toby Ellison-Scott <ellisonscotttoby@gmail.com>
2019-03-29 11:08:45 +0000

    Added the Jackson-databind dependency. Created a bean to read from the json file and bind data to Contact objects. Made a method that will carry out the databind automatically upon loading of app.

--- src/main/java/com/toby/json/JsonApplication.java
+++ src/main/java/com/toby/json/JsonApplication.java
@@ -1,13 +1,42 @@
 package com.toby.json;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.toby.json.service.ContactService;
+import com.toby.json.constants.Constants;
+import com.toby.json.domain.Contact;
 
 @SpringBootApplication
 public class JsonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(ContactService contactService) {
+		return args -> {
+			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<Contact>> typeReference = new TypeReference<List<Contact>>() {
+			};
+			InputStream inputStream = TypeReference.class.getResourceAsStream(Constants.JSON_LOCATION);
+			try {
+				List<Contact> users = mapper.readValue(inputStream, typeReference);
+				contactService.saveListOfContacts(users);
+				System.out.println(Constants.CONTACTS_SAVED);
+			} catch (IOException e) {
+				System.out.println(Constants.CONTACTS_NOT_SAVED + e.getMessage());
+			}
+		};
 	}
 
 }
---
+++ src/main/java/com/toby/json/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.toby.json.constants;
+
+public class Constants {
+
+	private Constants() {
+
+	}
+	
+	public static final String JSON_LOCATION = "/json/Contact_List.json";
+	
+	public static final String CONTACTS_SAVED = "Contacts Saved";
+	
+	public static final String CONTACTS_NOT_SAVED = "Contacts were not saved : ";
+}
