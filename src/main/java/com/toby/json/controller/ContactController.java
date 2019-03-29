ec1864a6f4768a2b7107c9f427920de9fd046332
Toby Ellison-Scott <ellisonscotttoby@gmail.com>
2019-03-29 10:41:52 +0000

    Created endpoint mapping to get list of contacts. Enabled Cross-origin requests to endpoints.

--- src/main/java/com/toby/json/controller/ContactController.java
+++ src/main/java/com/toby/json/controller/ContactController.java
@@ -1,5 +1,27 @@
 package com.toby.json.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.toby.json.domain.Contact;
+import com.toby.json.service.ContactService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("${endpoint.base}")
 public class ContactController {
+
+	private ContactService contactService;
+
+	public ContactController(ContactService contactService) {
+		this.contactService = contactService;
+	}
+
+	@GetMapping("${endpoint.get}")
+	public Iterable<Contact> getAllContacts() {
+		return contactService.getAllContacts();
+	}
 
 }
