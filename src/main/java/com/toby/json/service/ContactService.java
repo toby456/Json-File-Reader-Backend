a4c4895ce1670cc08d4fee07f1aadc6124d82e86
Toby Ellison-Scott <ellisonscotttoby@gmail.com>
2019-03-29 10:28:23 +0000

    Created Method to save json list to database. Created method to get json list from database.

--- src/main/java/com/toby/json/service/ContactService.java
+++ src/main/java/com/toby/json/service/ContactService.java
@@ -1,5 +1,27 @@
 package com.toby.json.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.toby.json.domain.Contact;
+import com.toby.json.repository.ContactRepo;
+
+@Service
 public class ContactService {
+
+	private ContactRepo contactRepo;
+
+	public ContactService(ContactRepo contactRepo) {
+		this.contactRepo = contactRepo;
+	}
+
+	public Iterable<Contact> getAllContacts() {
+		return contactRepo.findAll();
+	}
+
+	public Iterable<Contact> saveListOfContacts(List<Contact> contactList) {
+		return contactRepo.saveAll(contactList);
+	}
 
 }
